d2e3643dddc96aa452fc65ac1798fbf7a8d5c65f
sogore massire <massire.org@gmail.com>
2023-12-25 18:24:23 +0100

    this is the first commit

---
+++ src/main/java/com/cs/artfactonline/ArtfactOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.cs.artfactonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtfactOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtfactOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs/artfactonline/ArtfactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.artfactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtfactOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
